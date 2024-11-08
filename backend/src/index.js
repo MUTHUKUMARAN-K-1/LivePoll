af25c4b5fc08304f743db40e77aa476a8ce50e43
Manik Maity <manikmaity156@gmail.com>
2024-11-08 22:07:58 +0530

    FIX - Cors Bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,10 @@ const io = new Server(httpServer, {
 })
 
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json())
 app.get("/ping", (_req, res) => {
     res.json({ message: "pong" })
